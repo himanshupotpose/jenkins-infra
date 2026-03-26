e2c752246394dd30ea4cb127b08f520467fce890
Himanshu <himanshupotpose388@gmail.com>
2026-03-26 22:11:34 +0530

    fix again

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "region" {
+  description = "AWS region"
+  type        = string
+}
+
+variable "ami_id" {
+  description = "AMI ID"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+}
---
+++ vars/staging.tfvars
@@ -0,0 +1,3 @@
+region        = "ap-south-1"
+ami_id        = "ami-05d2d839d4f73aafb"
+instance_type = "c7i-flex.large"
